b5c1905b8c6cb0903eb36973be88bfaf30def3e9
Andreas Schörgenhumer <andischoe@gmail.com>
2022-09-28 13:33:30 +0200

    Adapt unit tests (missing in 074d380be38be885e034b697ec935d7ba434273c)

--- grading/test/ws2021/test_handson1lecturegrader.py
+++ grading/test/ws2021/test_handson1lecturegrader.py
@@ -3,8 +3,8 @@ import pandas as pd
 from graders.ws2021.handson1lecturegrader import HandsOn1LectureGrader
 from test.abstractgradertest import AbstractGraderTest
 
-QRETRY_COL = "Quiz: Retry Exam (Real)"
-COLUMNS = ["Quiz: Exam 1 (Real)", "Quiz: Exam 2 (Real)", QRETRY_COL, "expected_grade"]
+COLUMNS = ["Quiz: Exam 1 (Real)", "Quiz: Exam 2 (Real)",
+           "Quiz: Retry Exam (Real)", "Quiz: Retry Exam 2 (Real)", "expected_grade"]
 
 
 class HandsOn1LectureGraderTest(AbstractGraderTest):
@@ -13,65 +13,76 @@ class HandsOn1LectureGraderTest(AbstractGraderTest):
         return HandsOn1LectureGrader
     
     def test_create_grading_file_q1q2_threshold_negative(self):
-        points = pd.DataFrame([
+        self.assert_equal_grades(pd.DataFrame([
             # q1
-            [0, 100, "-", 5],
-            [39, 100, "-", 5],
-            ["-", 100, "-", 5],
+            [0, 100, "-", "-", 5],
+            [39, 100, "-", "-", 5],
+            ["-", 100, "-", "-", 5],
             # q2
-            [100, 0, "-", 5],
-            [100, 39, "-", 5],
-            [100, "-", "-", 5],
-        ], columns=COLUMNS)
-        self.assert_equal_grades(points)
-        self.assert_equal_grades(points.drop(columns=QRETRY_COL))
+            [100, 0, "-", "-", 5],
+            [100, 39, "-", "-", 5],
+            [100, "-", "-", "-", 5],
+        ], columns=COLUMNS))
     
     def test_create_grading_file_q1q2_total_negative(self):
-        points = pd.DataFrame([
-            [40, 59, "-", 5],
-            [49, 50, "-", 5],
-            [59, 40, "-", 5],
-            [50, 49, "-", 5],
-        ], columns=COLUMNS)
-        self.assert_equal_grades(points)
-        self.assert_equal_grades(points.drop(columns=QRETRY_COL))
+        self.assert_equal_grades(pd.DataFrame([
+            [40, 59, "-", "-", 5],
+            [49, 50, "-", "-", 5],
+            [59, 40, "-", "-", 5],
+            [50, 49, "-", "-", 5],
+        ], columns=COLUMNS))
     
     def test_create_grading_file_q1q2_total_positive(self):
-        points = pd.DataFrame([
-            [40, 60, "-", 4],
-            [60, 40, "-", 4],
-            [50, 50, "-", 4],
-            [100, 100, "-", 1],
-        ], columns=COLUMNS)
-        self.assert_equal_grades(points)
-        self.assert_equal_grades(points.drop(columns=QRETRY_COL))
+        self.assert_equal_grades(pd.DataFrame([
+            [40, 60, "-", "-", 4],
+            [60, 40, "-", "-", 4],
+            [50, 50, "-", "-", 4],
+            [100, 100, "-", "-", 1],
+        ], columns=COLUMNS))
     
     def test_create_grading_file_qretry_negative(self):
         self.assert_equal_grades(pd.DataFrame([
             # should be independent of q1 + q2
-            ["-", "-", 99, 5],
-            ["-", 100, 99, 5],
-            [100, "-", 99, 5],
-            [39, 100, 99, 5],
-            [100, 39, 99, 5],
-            [100, 100, 99, 5],
+            ["-", "-", 99, "-", 5],
+            ["-", 100, 99, "-", 5],
+            [100, "-", 99, "-", 5],
+            [39, 100, 99, "-", 5],
+            [100, 39, 99, "-", 5],
+            [100, 100, 99, "-", 5],
         ], columns=COLUMNS))
     
     def test_create_grading_file_qretry_positive(self):
         self.assert_equal_grades(pd.DataFrame([
             # should be independent of q1 + q2
-            ["-", "-", 100, 4],
-            ["-", 39, 100, 4],
-            ["-", 100, 100, 4],
-            [39, "-", 100, 4],
-            [100, "-", 100, 4],
-            [39, 100, 100, 4],
-            [100, 39, 100, 4],
-            [100, 100, 100, 4],
-            ["-", "-", 200, 1],
-            ["-", 100, 200, 1],
-            [100, "-", 200, 1],
-            [39, 100, 200, 1],
-            [100, 39, 200, 1],
-            [100, 100, 200, 1],
+            ["-", "-", 100, "-", 4],
+            ["-", 39, 100, "-", 4],
+            ["-", 100, 100, "-", 4],
+            [39, "-", 100, "-", 4],
+            [100, "-", 100, "-", 4],
+            [39, 100, 100, "-", 4],
+            [100, 39, 100, "-", 4],
+            [100, 100, 100, "-", 4],
+            ["-", "-", 200, "-", 1],
+            ["-", 100, 200, "-", 1],
+            [100, "-", 200, "-", 1],
+            [39, 100, 200, "-", 1],
+            [100, 39, 200, "-", 1],
+            [100, 100, 200, "-", 1],
+        ], columns=COLUMNS))
+    
+    def test_create_grading_file_multiple_exams(self):
+        # latest exam should take precedence
+        self.assert_equal_grades(pd.DataFrame([
+            [100, 100, 0, "-", 5],
+            [100, 0, 200, "-", 1],
+            [100, 100, "-", 0, 5],
+            [100, 0, "-", 200, 1],
+            [100, "-", 200, 0, 5],
+            [100, "-", 0, 200, 1],
+            [100, 100, 0, 0, 5],
+            [100, 0, 200, 0, 5],
+            [100, 100, 200, 0, 5],
+            [100, 100, 0, 200, 1],
+            [100, 0, 200, 200, 1],
+            [100, 100, 200, 200, 1],
         ], columns=COLUMNS))
