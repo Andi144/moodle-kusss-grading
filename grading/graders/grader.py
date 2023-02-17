4a6e4a9b7c19945d313c92570b480616a6c5a2c2
Andreas Schörgenhumer <andischoe@gmail.com>
2023-02-17 08:58:09 +0100

    Only warn if explicitly set (added in cd99c257668f9d30d9c5de5d63aafe4bcc075d07)
    Reason is that we mainly deal with joint Moodle courses, and this warning would basically be issued all the time when grading exercises (where, of course, the number of participants is much smaller than the combined Moodle participants)

--- grading/graders/grader.py
+++ grading/graders/grader.py
@@ -166,6 +166,7 @@ class Grader:
     
     def create_grading_file(self, kusss_participants_files: Union[str, list[str]],
                             row_filter: Callable[[pd.Series], bool] = None,
+                            warn_if_not_found_in_kusss_participants: bool = False,
                             input_sep: str = ";", matr_id_col: str = "Matrikelnummer", study_id_col: str = "SKZ",
                             output_sep: str = ";", header: bool = False, grading_file: str = None,
                             grade_col: str = "grade", grade_reason_col: str = "grade_reason",
@@ -184,6 +185,9 @@ class Grader:
             i.e., student entries, where True is returned. This function is applied after
             merging with the KUSSS participants and right before the grades are calculated.
             Default: None, i.e., all entries are used for grading
+        :param warn_if_not_found_in_kusss_participants: If True, a warning is issued in
+            case there are students in the main Moodle file that cannot be found in the
+            specified KUSSS participants (``kusss_participants_files``). Default: False
         :param input_sep: The separator character of the participants CSV input file(s).
             Default: ";"
         :param matr_id_col: The column name of the participants CSV input file(s) that
@@ -231,7 +235,7 @@ class Grader:
         self._print(f"size after merging with KUSSS participants {kdf.shape}: {df.shape}")
         if len(df) == 0:
             raise ValueError("no entries remain after merging with KUSSS participants")
-        elif len(df) < len(self.df):
+        elif len(df) < len(self.df) and warn_if_not_found_in_kusss_participants:
             diff = self.df[~self.df["ID number"].isin(kdf[matr_id_col])]
             assert len(diff) == len(self.df) - len(df)
             warnings.warn(f"the following {len(diff)} entries were not part of the KUSSS participants, so they cannot "
