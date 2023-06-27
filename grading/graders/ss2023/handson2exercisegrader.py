1c6491da8890e78212b6cf6681e7803931e77cc9
Andreas Schörgenhumer <andischoe@gmail.com>
2023-06-27 14:08:44 +0200

    Add Hands-on 2 graders

---
+++ grading/graders/ss2023/handson2exercisegrader.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+from graders import util
+from graders.grader import Grader
+
+MAX_POINTS = 600  # 6 assignments with 100 points each
+
+
+# TODO: 1:1 copy of ss2022
+class HandsOn2ExerciseGrader(Grader):
+    
+    def _create_grade_row(self, row: pd.Series) -> pd.Series:
+        # only one assignment can be skipped or graded with 0 points
+        # replace 0 points with NaN to make things easier with pd.Series.isna()
+        assignment_points = row[self.assignment_cols].replace(0, np.nan)
+        if assignment_points.isna().sum() > 1:
+            return pd.Series([5, "more than 1 assignment skipped/graded with 0 points"])
+        return util.create_grade(assignment_points.sum(), MAX_POINTS)
+
+
+if __name__ == "__main__":
+    args = util.get_grading_args_parser().parse_args()
+    util.args_sanity_check(args.moodle_file, args.kusss_participants_files, "handson2")
+    grader = HandsOn2ExerciseGrader(args.moodle_file)
+    gdf, gf = grader.create_grading_file(args.kusss_participants_files, grading_file=args.grading_file)
+    gdf.to_csv(gf.replace(".csv", "_FULL.csv"), index=False)
---
+++ grading/graders/ss2023/handson2lecturegrader.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+
+from graders import util
+from graders.grader import Grader
+
+MAX_POINTS = 100
+
+
+class HandsOn2LectureGrader(Grader):
+    
+    # TODO: identical code to, e.g., Python2LectureGrader (should extract to common base class, maybe with template
+    #  method to include optional bonus points for each of the three exams)
+    def _create_grade_row(self, row: pd.Series) -> pd.Series:
+        e1 = row["Quiz: Exam (Real)"]
+        e2 = row["Quiz: Retry Exam (Real)"]
+        e3 = row["Quiz: Retry Exam 2 (Real)"]
+        # most recent exam takes precedence
+        if not np.isnan(e3):
+            points = e3
+        elif not np.isnan(e2):
+            points = e2
+        else:
+            assert not np.isnan(e1)
+            points = e1
+        return util.create_grade(points, MAX_POINTS)
+
+
+if __name__ == "__main__":
+    args = util.get_grading_args_parser().parse_args()
+    util.args_sanity_check(args.moodle_file, args.kusss_participants_files, "handson2")
+    grader = HandsOn2LectureGrader(args.moodle_file)
+    gdf, gf = grader.create_grading_file(args.kusss_participants_files, grading_file=args.grading_file,
+                                         row_filter=lambda row: (~row[grader.quiz_cols].isna()).any(),
+                                         warn_if_not_found_in_kusss_participants=True)
+    gdf.to_csv(gf.replace(".csv", "_FULL.csv"), index=False)
